9b0ea4e6c16d7af7dd32626800660298c400589f
madhurrajput23 <105670693+madhurrajput23@users.noreply.github.com>
2022-12-31 17:20:32 +0530

    fetch product through api

--- components/products/Product.jsx
+++ components/products/Product.jsx
@@ -1,32 +1,52 @@
 import React from "react";
 import styles from "./Product.module.css";
 
-const Product = ({imgSrc, title, additionalPrice , actualPrice, rating }) => {
-  const discount = ((additionalPrice - actualPrice) / additionalPrice) * 100;
+const Product = ({
+  imgSrc,
+  title,
+  actualPrice,
+  rating,
+  discountPercentage,
+  inStock,
+}) => {
+  const additionalPrice = (actualPrice * 100) / (100 - discountPercentage);
   return (
     <div className={styles.product_container}>
       <div className={styles.product_top}>
         <img src={imgSrc} />
       </div>
-      <div className={styles.product_bottom}>
-        <div className={styles.product_info}>
-          <h3 className={styles.product_title}>{title}</h3>
-          <div>
-            <div className={styles.productinfo_row}>
-              <span>Rs {actualPrice}</span>
-              <span className={styles.additional_price}>Rs {additionalPrice}</span>
-              <span className={"color_primary"}>{discount.toFixed(0)}% off</span>
-            </div>
-            <div className={styles.productinfo_row}>
-              <span>Rating</span>
-              <span className={"color_red"}>{rating}★</span>
+      {inStock ? (
+        <div className={styles.product_bottom}>
+          <div className={styles.product_info}>
+            <h3 className={styles.product_title}>{title}</h3>
+            <div>
+              <div className={styles.productinfo_row}>
+                <span>Rs {actualPrice}</span>
+                <span className={styles.additional_price}>
+                  {additionalPrice.toFixed(0)}
+                </span>
+                <span className={"color_green"}>
+                  {discountPercentage.toFixed(0)}% off
+                </span>
+              </div>
+              <div className={styles.productinfo_row}>
+                <span>Rating</span>
+                <span className={rating > 4.2 ? "color_primary" : "color_red"}>
+                  {rating}★
+                </span>
+              </div>
             </div>
           </div>
+          <div className={styles.product_cta}>
+            <button className="primary_button">Add to Cart</button>
+          </div>
         </div>
-        <div className={styles.product_cta}>
-          <button className="primary_button">Add to Cart</button>
-        </div>
-      </div>
+      ) : (
+        <>
+          <h3 className={styles.product_title}>{title}</h3>
+          <div className={styles.product_unavailable}>Out of stock</div>
+        </>
+      )}
     </div>
   );
 };
--- pages/products/index.jsx
+++ pages/products/index.jsx
@@ -1,91 +1,36 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 import Product from "../../components/products/Product";
 import style from "./products.module.css";
 
 const products = () => {
+  const [productsList, setProductsList] = useState([]);
+  const fetchProducts = async () => {
+    try {
+      const res = await axios.get("https://dummyjson.com/products");
+      setProductsList(res.data.products);
+    } catch (error) {
+      console.log("failed to fetch products", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
   return (
     <div className={style.products_list}>
-      <Product
-        imgSrc={
-          "https://rukminim1.flixcart.com/image/612/612/khqllzk0-0/kit/w/w/b/pb-2000-combo-featherlite-2-shuttle-jsbk-201-jager-smith-original-imafxzscszbrdrp5.jpeg?q=70"
-        }
-        title={"ArrowMax Racket set"}
-        actualPrice={500}
-        additionalPrice={600}
-        rating={3.9}
-      />
-      <Product
-        imgSrc={
-          "https://rukminim1.flixcart.com/image/612/612/khqllzk0-0/kit/w/w/b/pb-2000-combo-featherlite-2-shuttle-jsbk-201-jager-smith-original-imafxzscszbrdrp5.jpeg?q=70"
-        }
-        title={"ArrowMax Racket set"}
-        actualPrice={500}
-        additionalPrice={600}
-        rating={3.9}
-      />
-      <Product
-        imgSrc={
-          "https://rukminim1.flixcart.com/image/612/612/khqllzk0-0/kit/w/w/b/pb-2000-combo-featherlite-2-shuttle-jsbk-201-jager-smith-original-imafxzscszbrdrp5.jpeg?q=70"
-        }
-        title={"ArrowMax Racket set"}
-        actualPrice={500}
-        additionalPrice={600}
-        rating={3.9}
-      />
-      <Product
-        imgSrc={
-          "https://rukminim1.flixcart.com/image/612/612/khqllzk0-0/kit/w/w/b/pb-2000-combo-featherlite-2-shuttle-jsbk-201-jager-smith-original-imafxzscszbrdrp5.jpeg?q=70"
-        }
-        title={"ArrowMax Racket set"}
-        actualPrice={500}
-        additionalPrice={600}
-        rating={3.9}
-      />
-      <Product
-        imgSrc={
-          "https://rukminim1.flixcart.com/image/612/612/khqllzk0-0/kit/w/w/b/pb-2000-combo-featherlite-2-shuttle-jsbk-201-jager-smith-original-imafxzscszbrdrp5.jpeg?q=70"
-        }
-        title={"ArrowMax Racket set"}
-        actualPrice={500}
-        additionalPrice={600}
-        rating={3.9}
-      />
-      <Product
-        imgSrc={
-          "https://rukminim1.flixcart.com/image/612/612/khqllzk0-0/kit/w/w/b/pb-2000-combo-featherlite-2-shuttle-jsbk-201-jager-smith-original-imafxzscszbrdrp5.jpeg?q=70"
-        }
-        title={"ArrowMax Racket set"}
-        actualPrice={500}
-        additionalPrice={600}
-        rating={3.9}
-      />
-      <Product
-        imgSrc={
-          "https://rukminim1.flixcart.com/image/612/612/khqllzk0-0/kit/w/w/b/pb-2000-combo-featherlite-2-shuttle-jsbk-201-jager-smith-original-imafxzscszbrdrp5.jpeg?q=70"
-        }
-        title={"ArrowMax Racket set"}
-        actualPrice={500}
-        additionalPrice={600}
-        rating={3.9}
-      />
-      <Product
-        imgSrc={
-          "https://rukminim1.flixcart.com/image/612/612/khqllzk0-0/kit/w/w/b/pb-2000-combo-featherlite-2-shuttle-jsbk-201-jager-smith-original-imafxzscszbrdrp5.jpeg?q=70"
-        }
-        title={"ArrowMax Racket set"}
-        actualPrice={500}
-        additionalPrice={600}
-        rating={3.9}
-      />
-      <Product
-        imgSrc={
-          "https://rukminim1.flixcart.com/image/612/612/khqllzk0-0/kit/w/w/b/pb-2000-combo-featherlite-2-shuttle-jsbk-201-jager-smith-original-imafxzscszbrdrp5.jpeg?q=70"
-        }
-        title={"ArrowMax Racket set"}
-        actualPrice={500}
-        additionalPrice={600}
-        rating={3.9}
-      />
+      {productsList?.map((product) => (
+        <Product
+          key={product.id}
+          imgSrc={product.thumbnail}
+          title={product.title}
+          actualPrice={product.price}
+          discountPercentage={product.discountPercentage}
+          rating={product.rating}
+          inStock={product.stock > 50 ? true : false}
+        />
+      ))}
     </div>
   );
 };
